57133537d0c9283a457e5c47040060c687f0134e
Honore-Pacifique <honore.bayisenge@bosa.fgov.be>
2025-03-16 08:34:32 +0100

    gilded-tros:  refactoring

--- Javascript/src/gilded-tros.js
+++ Javascript/src/gilded-tros.js
@@ -11,46 +11,50 @@ export class GildedTros {
     "B-DAWG Keychain",
   ];
 
+  #smellyItems = ["Duplicate Code", "Long Methods", "Ugly Variable Names"];
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
 
-      if (this.#isNormalItem(item)) {
+      //  At the end of each day our system lowers both values for every item
+      if (this.#isNormalItem(item) || this.#isSmellyItem(item)) {
         this.#decreaseQuality(item);
       } else {
+
+        // "Good Wine" actually increases in Quality the older it gets
         this.#increaseQuality(item);
 
-        if (item.name == "Backstage passes for Re:Factor") {
-          if (item.sellIn < 11) {
-            this.#increaseQuality(item);
-          }
+        // for very interesting conferences increases in Quality as its SellIn value approaches
+        if (this.#isBackstageItem(item)) {
 
-          if (item.sellIn < 6) {
-            this.#increaseQuality(item);
-          }
+          // Quality increases by 2 when there are 10 days or less
+          if (item.sellIn < 11) this.#increaseQuality(item);
+
+          // Quality increases by 3 when there are 5 days or less
+          if (item.sellIn < 6) this.#increaseQuality(item);
+          
         }
       }
 
       this.#decreaseSellIn(item);
 
+      // Once the sell by date has passed
       if (item.sellIn < 0) {
         
-        if (item.name != "Good Wine") {
+        if (!this.#isGoodWineItem(item)) {
 
-          if (
-            item.name != "Backstage passes for Re:Factor" ||
-            item.name != "Backstage passes for HAXX"
-          ) {
+          if (!this.#isBackstageItem(item)) {
+            // Quality degrades twice as fast
             this.#decreaseQuality(item);
-          } else {
-            item.quality = item.quality - item.quality;
           }
-
-
-
-
-
+          // "Backstage passes": Quality drops to 0 after the conference
+          else {
+            item.quality = 0;
+          }
         } else {
+          // "Good Wine": increases  Quality the older it gets
+          // Quality degrades twice as fast
           this.#increaseQuality(item);
         }
       }
@@ -73,16 +77,26 @@ export class GildedTros {
   }
 
   #isNormalItem(item) {
-    return !this.#specialTitles.includes(item.name);
+    return (
+      !this.#specialTitles.includes(item.name) &&
+      !this.#smellyItems.includes(item.name)
+    );
   }
 
-  #isBackstageItem(item){
-    retrun ['Backstage passes'].includes(item.name)
+  #isSmellyItem(item) {
+    return this.#smellyItems.includes(item.name)
+    
+  }
+
+  #isBackstageItem(item) {
+    return item.name.includes("Backstage passes");
   }
 
   #isLegendaryItem(item) {
-    return ["B-DAWG Keychain"].includes(item.name);
+    return item.name.includes("B-DAWG Keychain");
   }
 
-  
+  #isGoodWineItem(item) {
+    return item.name.includes("Good Wine");
+  }
 }
